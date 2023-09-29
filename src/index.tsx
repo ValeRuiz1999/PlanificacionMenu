c4929bf447c9276a0a557c2b648682ecaf10321e
ValeRuiz1999 <valeriaye2019@gmail.com>
2023-09-28 19:59:06 -0500

    Creación de navbar responsive

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const button = (document.querySelector('.button') as HTMLButtonElement)
+const nav = (document.querySelector('.nav') as HTMLElement)
+
+button.addEventListener('click', (): void => {
+  nav.classList.toggle('activo')
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +19,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
